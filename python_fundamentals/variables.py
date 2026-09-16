e509e9efc48b2cb2ced64b92d6828ed148d0c665
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2026-09-16 23:44:03 +0800

    Update variables.py

---
+++ python_fundamentals/variables.py
@@ -0,0 +1,19 @@
+name = "Nantha"
+age = 33
+
+print(name)
+print(age)
+
+salary = 8500.50
+is_developer = True
+middle_name = None
+
+name = "Nantha"
+age = 33
+salary = 8500.50
+is_developer = True
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_developer))
