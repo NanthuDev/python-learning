81539748042441ee51bcb9a92181069841b8b6c6
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2026-09-17 00:47:25 +0800

    Update variables.py

--- python_fundamentals/variables.py
+++ python_fundamentals/variables.py
@@ -18,11 +18,17 @@ age = 33
 salary = 8500.50
 is_developer = True
 
+# Python gives you type().
 print(type(name))
 print(type(age))
 print(type(salary))
 print(type(is_developer))
 
+
+
+# Python is dynamically typed
+# string name = "Nantha"
+# int age = 33
 value = 10
 
 print(type(value))
@@ -31,16 +37,26 @@ value = "hello"
 
 print(type(value))
 
+
+# variable cannot start with a number
+# - is interpreted as subtraction
 # 1name = "Nantha"
 # first-name = "Nantha"
 
 
+# Use snake_case:
 first_name = "Nantha"
 last_name = "Kumar"
 monthly_salary = 8500
 is_logged_in = True
 
+# Avoid:
+FirstName = "Nantha"
+MonthlySalary = 8500
+print(FirstName)
 
+
+# Variables can be calculated
 price = 100
 quantity = 3
 
@@ -58,3 +74,47 @@ final_price = subtotal + tax
 
 print(subtotal)
 print(final_price)
+
+
+# Variables can be changed
+age = 32
+
+print(age)
+
+age = 33
+
+print(age)
+
+# Multiple assignment
+name, age, city = "Nantha", 33, "Kuala Lumpur"
+
+print(name)
+print(age)
+print(city)
+
+# You can also assign the same value:
+x = y = z = 100
+
+print(x)
+print(y)
+print(z)
+
+# Variables and strings
+
+# Don't do this unnecessarily:
+name = "Nantha"
+
+print("My name is " + name)
+
+# Python's f-strings are much cleaner:
+name = "Nantha"
+age = 33
+
+print(f"My name is {name} and I am {age} years old.")
+
+# Variables + user input
+name = input("Enter your name: ")
+
+print(f"Hello, {name}!")
+
+# input() always returns a string
