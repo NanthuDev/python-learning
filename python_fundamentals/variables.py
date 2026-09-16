17d00dd88105943b4f6f508888c2341f37e5c3bc
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2026-09-17 00:03:24 +0800

    Update variables.py

--- python_fundamentals/variables.py
+++ python_fundamentals/variables.py
@@ -1,11 +1,16 @@
 name = "Nantha"
 age = 33
 
+
 print(name)
 print(age)
 
+# Python variables aren't restricted to one type.
+
 salary = 8500.50
 is_developer = True
+
+# None means there is currently no value.
 middle_name = None
 
 name = "Nantha"
@@ -17,3 +22,39 @@ print(type(name))
 print(type(age))
 print(type(salary))
 print(type(is_developer))
+
+value = 10
+
+print(type(value))
+
+value = "hello"
+
+print(type(value))
+
+# 1name = "Nantha"
+# first-name = "Nantha"
+
+
+first_name = "Nantha"
+last_name = "Kumar"
+monthly_salary = 8500
+is_logged_in = True
+
+
+price = 100
+quantity = 3
+
+total = price * quantity
+
+print(total)
+
+
+price = 100
+quantity = 3
+tax = 30
+
+subtotal = price * quantity
+final_price = subtotal + tax
+
+print(subtotal)
+print(final_price)
