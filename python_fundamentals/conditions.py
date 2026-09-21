3b30dfb7b09109cc0ade27ac61a89cf373b5083f
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2026-09-22 00:03:14 +0800

    Update conditions.py

--- python_fundamentals/conditions.py
+++ python_fundamentals/conditions.py
@@ -38,3 +38,20 @@ if password == 'temp':
     print("its correct")
 else:
     print("not correct")
+
+
+
+print("If...Elif")
+
+marks = 92
+
+if marks >= 90:
+    print("A Grade")
+elif marks >= 70:
+    print("B Grade")
+elif marks >= 60:
+    print("C Grade")
+elif marks >= 30:
+    print("D Grade")
+else:
+    print("Fail")
