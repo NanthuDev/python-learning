8f67b743e249f654f4b44a33a648fcc13a8444b6
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2026-09-21 23:59:09 +0800

    Update conditions.py

--- python_fundamentals/conditions.py
+++ python_fundamentals/conditions.py
@@ -10,3 +10,21 @@ if age >= 18:
 temperature = 30
 if temperature >= 20:
     print("cit is hotter")
+
+
+print("conditions")
+
+age = 25
+
+print(age == 25)
+print(age > 18)
+print(age < 18)
+print(age != 30)
+
+
+age = 16
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
