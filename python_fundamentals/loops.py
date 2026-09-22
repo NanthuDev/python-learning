6bc93d652dbf1cf02a478aaed2143457f3bb2909
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2026-09-23 00:47:10 +0800

    Update loops.py

--- python_fundamentals/loops.py
+++ python_fundamentals/loops.py
@@ -11,3 +11,10 @@ for i in range(3):
 for i in range(1,11):
     if i % 2 == 0:
         print("even")
+
+ages = [15, 22, 17, 30, 14, 25]
+for age in ages:
+    if age >= 18:
+        print(age)
+
+        
