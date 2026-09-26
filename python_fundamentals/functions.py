8366e772623c5222b0af82a592ad52019d7a8536
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2026-09-26 22:52:42 +0800

    Update functions.py

--- python_fundamentals/functions.py
+++ python_fundamentals/functions.py
@@ -19,9 +19,33 @@ def say_hello():
 say_hello()
 
 
+# Multiple parameters
 def introduce(name, age):
     print("My name is", name)
     print("I am", age, "years old")
 
 
 introduce("John", 25)
+
+# return
+
+def add(a, b):
+    return a + b
+
+
+result = add(10, 20)
+
+print(result)
+
+# Function with a condition
+def check_age(age):
+
+    if age >= 18:
+        return "Adult"
+    else:
+        return "Minor"
+
+
+result = check_age(25)
+
+print(result)
