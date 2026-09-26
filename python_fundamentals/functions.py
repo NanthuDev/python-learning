25b7aa0da55ba13499b51a915469b73eaca1ab64
Nanthakumar <35912811+NanthuDev@users.noreply.github.com>
2026-09-26 22:51:19 +0800

    Update functions.py

--- python_fundamentals/functions.py
+++ python_fundamentals/functions.py
@@ -10,3 +10,18 @@ greet("Sarah")
     # code
 # calling it
 # function_name(arguments)
+
+
+def say_hello():
+    print("Hello!")
+
+
+say_hello()
+
+
+def introduce(name, age):
+    print("My name is", name)
+    print("I am", age, "years old")
+
+
+introduce("John", 25)
